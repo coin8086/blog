f4b185442f4e3997f792d3b623670eb6138eb150
Robert Zhang <louirobert@gmail.com>
2017-05-23 21:29:47 +0800

    Sort tags on name for tag_cloud2

--- _plugins/jekyll-tagging-patch.rb
+++ _plugins/jekyll-tagging-patch.rb
@@ -3,10 +3,14 @@ require 'jekyll/tagging'
 module Jekyll
   module TaggingFilters2
     def tag_cloud2(site)
-      site.tags.map do |tag, posts|
+      tags_with_count(site).map do |tag, count|
         # tag_link is from Gem jekyll-tagging.
-        tag_link("#{tag}(#{posts.size})", tag_url(tag))
+        tag_link("#{tag}(#{count})", tag_url(tag))
       end.join(' ')
+    end
+
+    def tags_with_count(site)
+      @tags_with_count ||= site.tags.sort_by {|e| e[0]}.map {|tag, posts| [tag, posts.size] }
     end
   end
 end
